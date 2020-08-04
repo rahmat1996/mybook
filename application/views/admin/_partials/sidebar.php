967f33f77e0caf9e59ff01893a2a069f0ad14e28
rahmat1996 <rahmatbatam14@gmail.com>
2020-08-04 13:29:32 +0700

    fix link to home on admin panel

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
-	<a href="../../index3.html" class="brand-link">
+	<a href="<?php echo site_url(); ?>" class="brand-link">
 		<div class="brand-text font-weight-light" style="text-align: center;font-size: 24px;"><b>My</b>Book</div>
 	</a>
 
